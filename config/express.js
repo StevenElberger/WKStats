48779dba9492f54f794ea251b0e917cebb2d1c68
Steven Elberger <stevenelberger@gmail.com>
2016-07-23 15:20:23 -0700

    Removes files that were unused and will be re-added

--- config/express.js
+++ config/express.js
@@ -8,25 +8,24 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var express = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+express.set('views', path.join(__dirname, 'views'));
+express.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//express.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+express.use(logger('dev'));
+express.use(bodyParser.json());
+express.use(bodyParser.urlencoded({ extended: false }));
+express.use(cookieParser());
+express.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+express.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+express.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -36,8 +35,8 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (express.get('env') === 'development') {
+  express.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -48,7 +47,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+express.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -57,4 +56,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+module.exports = express;
