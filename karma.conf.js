76812db820fcb054503ef77211d87be681e25065
Steven Elberger <stevenelberger@gmail.com>
2016-07-31 14:09:40 -0700

    Fixes file name in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'testPrep.js',
-      'WKW.js',
+      'wkw.js',
       'app.js',
       'app.config.js',
       'home/home.module.js',
